4715cae6ced6650f2b7e0ffc97f50ccfa03315d5
Nishan Karki <karki.nishann@gmail.com>
2023-03-31 00:26:56 +0200

    changes

--- client/api-client.py
+++ client/api-client.py
@@ -1,5 +1,19 @@
 
 import json
+<<<<<<< HEAD
+import linecache
+import requests
+# with open("./data/output.txt") as f:
+#     count = 0
+#     for line in f:
+#         try:
+#             json.loads(line)
+#             count+=1
+#         except json.JSONDecodeError:
+#             pass
+start = 1
+end = 100
+=======
 
 with open("./data/output.txt") as f:
     count = 0
@@ -9,5 +23,6 @@ with open("./data/output.txt") as f:
             count+=1
         except json.JSONDecodeError:
             pass
+>>>>>>> parent of e79e96f (changes)
 
 print(f"Json has: {count}")
