9e5f6d501ae33db16a6336fd0918b5c6b78a617a
Nishan Karki <karki.nishann@gmail.com>
2023-03-31 15:36:36 +0200

    changes

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -8,5 +8,43 @@ import pymongo
 
 # Connect with MongoDB:
 myclient = pymongo.MongoClient("mongodb://localhost:27017/",username='root',password='example')
+mydb = myclient["docstreaming"]
+mycol = mydb["invoices"]
+
+cust_id = st.sidebar.text_input("CustomerID")
+
+if cust_id:
+
+    myquery = {"CustomerID": cust_id}
+    mydoc = mycol.find( myquery , { "_id": 0, "StockCode": 0, "Description": 0, "Quantity": 0, "Country": 0, "UnitPrice": 0})
+
+    # create dataframe from resulting documents to use drop_duplicates
+    df = DataFrame(mydoc)
+    
+    # drop duplicates, but keep the first one
+    df.drop_duplicates(subset ="InvoiceNo", keep = 'first', inplace = True)
+
+    # Add the table with a headline
+    st.header("Output Customer Invoices")
+    table2 = st.dataframe(data=df) 
+    
+
+# Below the fist chart add a input field for the invoice number
+inv_no = st.sidebar.text_input("InvoiceNo:")
 
 
+# if enter has been used on the input field 
+if inv_no:
+    
+    myquery = {"InvoiceNo": inv_no}
+    mydoc = mycol.find( myquery, { "_id": 0, "InvoiceDate": 0, "Country": 0, "CustomerID": 0 })
+
+    # create the dataframe
+    df = DataFrame(mydoc)
+
+    # reindex it so that the columns are order lexicographically 
+    reindexed = df.reindex(sorted(df.columns), axis=1)
+
+    # Add the table with a headline
+    st.header("Output by Invoice ID")
+    table2 = st.dataframe(data=reindexed) 
